4b7064feb2176e6e492291df61b96628aab96ee9
Julian Lemmerich <41118534+JM-Lemmi@users.noreply.github.com>
2021-11-10 14:52:59 +0100

    fix #56 handling multiple bodies now chooses ony text/plain and discards all others

--- src/zimbraweb/emlparsing.py
+++ src/zimbraweb/emlparsing.py
@@ -29,11 +29,30 @@ def parse_eml(user: ZimbraUser, eml: str) -> Tuple[bytes, str]:
         dict_mail = {}
         dict_mail['to'] = parsed['To']
         dict_mail['subject'] = parsed['Subject']
+        dict_mail['body'] = []  # we later only want one entry in body, but first we collect all body entries to then get the one we want
         dict_mail['attachments'] = []
-        return user.generate_webkit_payload(**parse_multipart_eml(user, parsed.get_payload(), dict_mail))
+
+        # parsing body and attachments to dictionary
+        dict_mail = parse_multipart_eml(user, parsed.get_payload(), dict_mail)
+
+        # at this point dict_mail['body'] is a list of email.message.Message objects
+
+        # parsing bodies
+        if len(dict_mail['body']) == 0:  # no body
+            dict_mail['body'] = ""
+        elif len(dict_mail['body']) == 1:  # one body
+            dict_mail['body'] = dict_mail['body'][0].get_payload()
+        elif len(dict_mail['body']) > 1:  # more than one body
+            for b in dict_mail['body']:
+                if b.get('Content-Type')[:b.get('Content-Type').find(";")] == "text/plain":
+                    body = b.get_payload()
+            dict_mail['body'] = body
+
+        return user.generate_webkit_payload(**dict_mail)
 
     else:
         raise TypeError()
+        # dont know when this happens, but just to be sure
 
 
 def parse_multipart_eml(user: ZimbraUser, payload: List[email.message.Message], dict_mail: Dict[str, Any]) -> Dict[str, Any]:
@@ -56,7 +75,7 @@ def parse_multipart_eml(user: ZimbraUser, payload: List[email.message.Message], 
         else:
             # body
             if "attachment" not in p.get('Content-Disposition', ''):
-                dict_mail['body'] = p.get_payload()
+                dict_mail['body'].append(p)
 
             # attachment
             if "attachment" in p.get('Content-Disposition', ''):
