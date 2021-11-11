c0df564abf5689744e1c97add80a6e86133523b5
Frederik Reiter <kontakt@frederikreiter.de>
2021-11-11 14:43:08 +0100

    Fix Tests to have a unique Message-ID header every time

--- tests/test_zimbra.py
+++ tests/test_zimbra.py
@@ -1,5 +1,6 @@
 import os
 import pkg_resources
+import uuid
 
 import pytest
 from zimbraweb import ZimbraUser, WebkitAttachment, emlparsing
@@ -52,20 +53,23 @@ def test_attachment_too_large(zimbra_user: ZimbraUser, identifier: str):
 
 def test_send_plain_eml(zimbra_user: ZimbraUser, identifier: str):
     eml = pkg_resources.resource_stream(__name__, "templates/plaintext.eml").read().decode("utf8")
-    response = zimbra_user.send_eml(eml.format(identifier=identifier, username=zimbra_user.session_data.username))
+    msg_id = uuid.uuid4()
+    response = zimbra_user.send_eml(eml.format(identifier=identifier, username=zimbra_user.session_data.username, messageid=msg_id))
     assert response.success
     assert response.message == "Ihre Mail wurde gesendet."
 
 
 def test_send_mixed_eml(zimbra_user: ZimbraUser, identifier: str):
     eml = pkg_resources.resource_stream(__name__, "templates/html.eml").read().decode("utf8")
+    msg_id = uuid.uuid4()
     with pytest.raises(emlparsing.ContentTypeNotSupportedError):
-        zimbra_user.send_eml(eml.format(identifier=identifier, username=zimbra_user.session_data.username))
+        zimbra_user.send_eml(eml.format(identifier=identifier, username=zimbra_user.session_data.username, messageid=msg_id))
 
 
 def test_send_attachment_eml(zimbra_user: ZimbraUser, identifier: str):
     eml = pkg_resources.resource_stream(__name__, "templates/attachments.eml").read().decode("utf8")
-    response = zimbra_user.send_eml(eml.format(identifier=identifier, username=zimbra_user.session_data.username))
+    msg_id = uuid.uuid4()
+    response = zimbra_user.send_eml(eml.format(identifier=identifier, username=zimbra_user.session_data.username, messageid=msg_id))
     assert response.success
     assert response.message == "Ihre Mail wurde gesendet."
 
