df57ea0d4591f8fee7ce2c3b9fea3b3cac90e8fe
Frederik Reiter <kontakt@frederikreiter.de>
2021-11-11 12:58:12 +0100

    Refactor EML parsing
    The eml parsing module now does not need a ZimbraUser instance which means its better seperated from the ZimbraUser class.
    Sending an EML now happens with ZimbraUser.send_eml(emlstr) which calls emlparsing.parse_eml(emlstr) and then sends the resulting
    parameters with ZimbraUser.send_raw_payload

--- tests/test_zimbra.py
+++ tests/test_zimbra.py
@@ -2,8 +2,7 @@ import os
 import pkg_resources
 
 import pytest
-from zimbraweb import ZimbraUser, WebkitAttachment
-import zimbraweb.emlparsing
+from zimbraweb import ZimbraUser, WebkitAttachment, emlparsing
 
 
 def test_failing_authentication():
@@ -51,26 +50,22 @@ def test_attachment_too_large(zimbra_user: ZimbraUser, identifier: str):
     assert response.message == "Anhang ist zu groß."
 
 
-def test_send_plaineml(zimbra_user: ZimbraUser, identifier: str):
-    eml = pkg_resources.resource_stream(__name__, "templates/simplemail.eml").read().decode("utf8")
-    payload, boundary = zimbraweb.emlparsing.parse_eml(zimbra_user, eml)
-    response = zimbra_user.send_raw_payload(payload, boundary)
+def test_send_plain_eml(zimbra_user: ZimbraUser, identifier: str):
+    eml = pkg_resources.resource_stream(__name__, "templates/plaintext.eml").read().decode("utf8")
+    response = zimbra_user.send_eml(eml.format(identifier=identifier, username=zimbra_user.session_data.username))
     assert response.success
     assert response.message == "Ihre Mail wurde gesendet."
 
 
-def test_send_outlookeml(zimbra_user: ZimbraUser, identifier: str):
-    eml = pkg_resources.resource_stream(__name__, "templates/outlookmail.eml").read().decode("utf8")
-    payload, boundary = zimbraweb.emlparsing.parse_eml(zimbra_user, eml)
-    response = zimbra_user.send_raw_payload(payload, boundary)
-    assert response.success
-    assert response.message == "Ihre Mail wurde gesendet."
+def test_send_mixed_eml(zimbra_user: ZimbraUser, identifier: str):
+    eml = pkg_resources.resource_stream(__name__, "templates/html.eml").read().decode("utf8")
+    with pytest.raises(emlparsing.ContentTypeNotSupportedError):
+        zimbra_user.send_eml(eml.format(identifier=identifier, username=zimbra_user.session_data.username))
 
 
-def test_send_attachmenteml(zimbra_user: ZimbraUser, identifier: str):
-    eml = pkg_resources.resource_stream(__name__, "templates/attachmentmail.eml").read().decode("utf8")
-    payload, boundary = zimbraweb.emlparsing.parse_eml(zimbra_user, eml)
-    response = zimbra_user.send_raw_payload(payload, boundary)
+def test_send_attachment_eml(zimbra_user: ZimbraUser, identifier: str):
+    eml = pkg_resources.resource_stream(__name__, "templates/attachments.eml").read().decode("utf8")
+    response = zimbra_user.send_eml(eml.format(identifier=identifier, username=zimbra_user.session_data.username))
     assert response.success
     assert response.message == "Ihre Mail wurde gesendet."
 
